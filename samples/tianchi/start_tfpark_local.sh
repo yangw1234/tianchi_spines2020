e31ca07f08002cf2eff93931fb2ba72429f19325
Wang, Yang <yang3.wang@intel.com>
2020-08-10 16:00:04 +0800

    refine scripts

--- samples/tianchi/start_tfpark_local.sh
+++ samples/tianchi/start_tfpark_local.sh
@@ -4,7 +4,7 @@ export OMP_NUM_THREADS=24
 export MASTER=local[4]
 export SPARK_DRIVER_MEMORY=100g
 
-export ANALYTICS_ZOO_HOME=/home/cpx/yang/dist/
-TF_LIBS=/home/cpx/yang/dist/lib/linux-x86_64
+export ANALYTICS_ZOO_HOME=/home/cpx/yang/dist
+TF_LIBS=$ANALYTICS_ZOO_HOME/lib/linux-x86_64
 bash $ANALYTICS_ZOO_HOME/bin/spark-submit-python-with-zoo.sh --master $MASTER --driver-memory 80g --executor-memory 80g --driver-java-options "-Djava.library.path=${TF_LIBS}" train_spines2.py
 
